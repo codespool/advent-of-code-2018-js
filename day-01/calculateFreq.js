1f551fddda79ef16b48b25d7ac291bccc92432dc
Saša Pul <sasa.pul@freska.fi>
2018-12-10 01:47:04 +0200

    revert recursion

--- day-01/calculateFreq.js
+++ day-01/calculateFreq.js
@@ -10,14 +10,16 @@ function calculateFreq(freqList) {
 function findFirstDuplicate(
   freqModifiers,
   currentSum = 0,
-  freqList = new Set([0]),
-  currentIndex = 0
+  freqList = new Set([0])
 ) {
-  const sum = currentSum + freqModifiers[currentIndex];
-  if (freqList.has(sum)) return sum;
-  freqList.add(sum);
-  const newIndex = freqModifiers[currentIndex + 1] ? currentIndex + 1 : 0;
-  return findFirstDuplicate(freqModifiers, sum, freqList, newIndex);
+  const uniqueFreqList = freqList;
+  let sum = currentSum;
+  for (let i = 0, max = freqModifiers.length; i < max; i += 1) {
+    sum += freqModifiers[i];
+    if (uniqueFreqList.has(sum)) return sum;
+    uniqueFreqList.add(sum);
+  }
+  return findFirstDuplicate(freqModifiers, sum, uniqueFreqList);
 }
 module.exports = {
   getIntArray,
