44fa0124e483f64dd56f47ff733e6492f558bfed
Saša Pul <sasa.pul@freska.fi>
2018-12-10 10:57:58 +0200

    day 01 part 1 solution

--- day-02/findChecksum.js
+++ day-02/findChecksum.js
@@ -1,5 +1,31 @@
 function findChecksum(stringList) {
-  return 0;
+  let pairsCount = 0;
+  let tripletsCount = 0;
+
+  stringList.forEach(element => {
+    const split = element.split('');
+    const map = {};
+    let foundPair = false;
+    let foundTriplet = false;
+    split.forEach(char => {
+      if (map[char]) {
+        map[char] += 1;
+      } else {
+        map[char] = 1;
+      }
+    });
+    Object.values(map).forEach(value => {
+      if (value === 2 && !foundPair) {
+        pairsCount += 1;
+        foundPair = true;
+      }
+      if (value === 3 && !foundTriplet) {
+        tripletsCount += 1;
+        foundTriplet = true;
+      }
+    });
+  });
+  return pairsCount * tripletsCount;
 }
 
 module.exports = {
